7dfad3c3b77b16c431afbb5d14abd8ed91e105e6
trushonlyyou <trushonlyyou@tnctec.co.kr>
2025-05-29 23:59:26 +0900

    Batch Guide Project init

---
+++ src/main/java/com/lunghwan/batch/guide/BatchGuideApplication.java
@@ -0,0 +1,13 @@
+package com.lunghwan.batch.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunghwan/batch/guide/BatchGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunghwan.batch.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
